ed42a14f4388c31d4dda1be3894f0bd76e5fb304
SuoCaiyuan <cysuo@cupdata.com>
2020-05-28 21:34:14 +0800

    chapter7 pool done

--- chapter7/pool/main/main.go
+++ chapter7/pool/main/main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"math/rand"
+	"runbird-go-inaction/chapter7/pool"
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
+const (
+	maxGoroutine   = 25
+	pooledResource = 2
+)
+
+//模拟被共享的资源
+type dbConnection struct {
+	ID int32
+}
+
+func (conn *dbConnection) Close() error {
+	log.Println("Close: Connection", conn.ID)
+	return nil
+}
+
+var idCounter int32
+
+func createConnection() (io.Closer, error) {
+	id := atomic.AddInt32(&idCounter, 1)
+	log.Println("Create: New Connection", id)
+	return &dbConnection{id}, nil
+}
+
+func performQueries(query int, p *pool.Pool) {
+	conn, err := p.Accquire()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer p.Release(conn)
+
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	log.Printf("QID[%d] CID[%d]\n", query, conn.(*dbConnection).ID)
+}
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(maxGoroutine)
+
+	//创建用来管理连接的池
+	p, err := pool.New(createConnection, pooledResource)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for query := 0; query < maxGoroutine; query++ {
+		go func(q int) {
+			performQueries(q, p)
+			wg.Done()
+		}(query)
+	}
+	wg.Wait()
+
+	log.Println("Shutdown Program.")
+	p.Close()
 }
